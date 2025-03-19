9160e7c1ee9ba5ed254da7c4771dbb8fbba3c6f9
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-19 12:41:24 +0000

    Add Handling Promise States. Page 187

--- src/components/GitHubUser.js
+++ src/components/GitHubUser.js
@@ -45,70 +45,39 @@ export default GitHubUser;
 */
 
 import React, { useState, useEffect } from "react";
-const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
-const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
-function GitHubUser({ login }) {
-  const [data, setData] = useState(loadJSON(`user:${login}`));
-  useEffect(() => {
-    if (!data) return;
-    if (data.login === login) return;
-    const { name, avatar_url, location } = data;
-    saveJSON(`user:${login}`, {
-      name,
-      login,
-      avatar_url,
-      location,
-    });
-  }, [data]);
-  useEffect(() => {
-    if (!login) return;
-    if (data && data.login === login) return;
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .catch(console.error);
-  }, [login]);
-  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
-  return null;
-}
 
-export default GitHubUser;
-
-/*
-import React, { useState, useEffect } from "react";
-
-const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
-const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+//const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
+//const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
 function GitHubUser({ login }) {
-  const [data, setData] = useState(loadJSON(`user:${login}`));
-
-  useEffect(() => {
-    if (!data) return;
-    if (data.login === login) return;
-    const { name, avatar_url, location } = data;
-    saveJSON(`user:${login}`, { name, login, avatar_url, location });
-  }, [data]);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!login) return;
-    if (data && data.login === login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
+      .then((data) => data.json())
       .then(setData)
-      .catch(console.error);
+      .then(() => setLoading(false))
+      .catch(setError);
   }, [login]);
 
-  if (!data) return <p>Loading...</p>;
+  if (loading) return <h1>loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
 
   return (
-    <div style={{ border: "1px solid #ddd", padding: "10px", borderRadius: "5px", width: "300px" }}>
-      <h2>{data.name || "No Name"}</h2>
-      <img src={data.avatar_url} alt={data.name} style={{ width: "100px", borderRadius: "50%" }} />
-      <p>Location: {data.location || "Unknown"}</p>
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
     </div>
   );
 }
 
 export default GitHubUser;
-*/
