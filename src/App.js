8834aa8a9c74b421f3ae9252d122759c0868b928
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-19 13:08:08 +0000

    Add example Render props. Page 190

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from "react";
-import GitHubUser from "./components/GitHubUser";
+
+const tahoe_peaks = [
+  { name: "Freel Peak", elevator: 10891 },
+  { name: "Monument Peak", elevator: 10067 },
+  { name: "Pyramid Peak", elevator: 9983 },
+  { name: "Mt. Tallac", elevator: 9735 },
+];
+
+function List({ data = [], renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
 
 export default function App() {
   return (
-    <div>
-      <h1>GitHub User Info</h1>
-      <GitHubUser login="moonhigway" />
-    </div>
+    <ul>
+      <List
+        data={tahoe_peaks}
+        renderEmpty={<p>This list is empty</p>}
+        renderItem={(item) => (
+          <>
+            {item.name} — {item.elevator.toLocaleString()}ft
+          </>
+        )}
+      />
+      {tahoe_peaks.map((peak, i) => (
+        <li key={i}>
+          {peak.name} - {peak.elevator.toLocaleString()}
+        </li>
+      ))}
+    </ul>
   );
 }
