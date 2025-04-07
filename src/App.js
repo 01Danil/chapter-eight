049d52d9f0afdd3ac8d845719b371a986b60069e
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-04-07 10:58:19 +0100

    Add example saveJSON. Page 183

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
 import React from "react";
-
-const tahoe_peaks = [
-  { name: "Freel Peak", elevator: 10891 },
-  { name: "Monument Peak", elevator: 10067 },
-  { name: "Pyramid Peak", elevator: 9983 },
-  { name: "Mt. Tallac", elevator: 9735 },
-];
-
-function List({ data = [], renderItem, renderEmpty }) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item, i) => (
-        <li key={i}>{renderItem(item)}</li>
-      ))}
-    </ul>
-  );
-}
+import GitHubUser from "./components/GitHubUser";
 
 export default function App() {
-  return (
-    <ul>
-      <List
-        data={tahoe_peaks}
-        renderEmpty={<p>This list is empty</p>}
-        renderItem={(item) => (
-          <>
-            {item.name} — {item.elevator.toLocaleString()}ft
-          </>
-        )}
-      />
-      {tahoe_peaks.map((peak, i) => (
-        <li key={i}>
-          {peak.name} - {peak.elevator.toLocaleString()}
-        </li>
-      ))}
-    </ul>
-  );
+  return <GitHubUser login="moonhighway" />;
 }
--- src/components/GitHubUser.js
+++ src/components/GitHubUser.js
@@ -44,6 +44,7 @@ function GitHubUser({ login }) {
 export default GitHubUser;
 */
 
+/*
 import React, { useState, useEffect } from "react";
 
 //const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
@@ -81,3 +82,47 @@ function GitHubUser({ login }) {
 }
 
 export default GitHubUser;
+*/
+
+/*
+import React, { useState, useEffect } from "react";
+
+export default function GitHubUser({ login }) {
+  const [data, setData] = useState();
+  useEffect(() => {
+    if (!login) return;
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .catch(console.error);
+  }, [login]);
+
+  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+
+  return null;
+}
+*/
+
+/*
+const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+*/
+
+import React, { useState, useEffect } from "react";
+
+export default function GitHubUser({ login }) {
+  const [data, setData] = useState(loadJSON(`user:${login}`));
+  useEffect(() => {
+    if (!data) return;
+    if (data.login === login) return;
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location,
+    });
+  }, [data]);
+
+  return null;
+}
