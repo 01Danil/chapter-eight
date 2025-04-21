2b25012a106964b6824896f3b9b1bdaf2ca1c426
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-04-21 11:09:15 +0100

    Add example BigList. Page 192

--- src/App.js
+++ src/App.js
@@ -1,6 +1,97 @@
+/*
 import React from "react";
-import GitHubUser from "./components/GitHubUser";
+
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891 },
+  { name: "Monument Peak", elevation: 10067 },
+  { name: "Pyramid Peak", elevation: 9983 },
+  { name: "Mt. Tallac", elevation: 9735 },
+];
 
 export default function App() {
-  return <GitHubUser login="moonhighway" />;
+  return (
+    <ul>
+      {tahoe_peaks.map((peak, i) => (
+        <li key={i}>
+          {peak.name} - {peak.elevation.toLocaleString()}ft
+        </li>
+      ))}
+    </ul>
+  );
 }
+	*/
+
+/*	
+import React from "react";
+
+function List({ data = [], renderEmpty }) {
+  if (!data.length) return renderEmpty;
+  return <p>{data.length} items</p>;
+}
+
+export default function App() {
+  return <List renderEmpty={<p>This list is empty</p>} />;
+}
+*/
+
+/*
+import React from "react";
+
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891 },
+  { name: "Monument Peak", elevation: 10067 },
+  { name: "Pyramid Peak", elevation: 9983 },
+  { name: "Mt. Tallac", elevation: 9735 },
+];
+
+export default function App() {
+  return <List data={tahoe_peaks} renderEmpty={<p>This list is empty</p>} />;
+}
+*/
+
+/*
+import React from "react";
+
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891 },
+  { name: "Monument Peak", elevation: 10067 },
+  { name: "Pyramid Peak", elevation: 9983 },
+  { name: "Mt. Tallac", elevation: 9735 },
+];
+
+export default function App() {
+  return (
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation.toLocaleString()}ft
+        </>
+      )}
+    />
+  );
+}
+
+function List({ data = [], renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
+*/
+
+// Виртуализированные списки
+
+import faker from "faker";
+
+const bigList = [...Array(5000)].map(() => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  avatar: faker.internet.avatar(),
+}));
