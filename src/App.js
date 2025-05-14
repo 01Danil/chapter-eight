903781dc2b36618a565a2bd4e26c3586ef6280a6
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-05-14 11:15:36 +0100

    Add example Render props. Page 187

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ function List({ data = [], renderItem, renderEmpty }) {
 */
 
 // Виртуализированные списки
-
+/*
 import faker from "faker";
 
 const bigList = [...Array(5000)].map(() => ({
@@ -95,3 +95,83 @@ const bigList = [...Array(5000)].map(() => ({
   email: faker.internet.email(),
   avatar: faker.internet.avatar(),
 }));
+*/
+
+//import React from "react";
+/*
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891 },
+  { name: "Monument Peak", elevation: 10067 },
+  { name: "Pyramid Peak", elevation: 9983 },
+  { name: "Mt. Tallac", elevation: 9735 },
+];
+
+export default function App() {
+  return (
+    <ul>
+      {tahoe_peaks.map((peak, i) => (
+        <li key={i}>
+          {peak.name} - {peak.elevation.toLocaleString()}ft
+        </li>
+      ))}
+    </ul>
+  );
+}
+*/
+
+/*
+function List({ data = [], renderEmpty }) {
+  if (!data.length) return renderEmpty;
+  return <p>{data.length} items</p>;
+}
+
+export default function App() {
+  return <List renderEmpty={<p>This list is empty</p>} />;
+}
+*/
+
+/*
+import React from "react";
+
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891 },
+  { name: "Monument Peak", elevation: 10067 },
+  { name: "Pyramid Peak", elevation: 9983 },
+  { name: "Mt. Tallac", elevation: 9735 },
+];
+
+export default function App() {
+	return (
+		<List 
+			data = {tahoe_peaks}
+			renderEmpty={<p>This list is empty</p>}
+		/>
+	)
+}
+*/
+
+export default function App() {
+  return (
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation.toLocaleString()}ft
+        </>
+      )}
+    />
+  );
+}
+
+function List({ data = [], renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
